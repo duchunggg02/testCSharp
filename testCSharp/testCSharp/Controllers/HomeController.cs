90621803d3d86f37a916ef8627acf0cad3709eed
duchunggg02 <nguyenduchung.02092002@gmail.com>
2024-07-25 15:18:16 +0700

    init project

---
+++ testCSharp/testCSharp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testCSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string FilePath
+        {
+            get { return Server.MapPath("~/App_Data/UserContent.txt"); }
+        }
+
+        public ActionResult Index()
+        {
+            var contentList = LoadContent();
+            return View(contentList);
+        }
+
+        [HttpPost]
+        public ActionResult AddContent(string content)
+        {
+            if (!string.IsNullOrEmpty(content))
+            {
+                SaveContent(content);
+            }
+            return RedirectToAction("Index");
+        }
+
+        private void SaveContent(string content)
+        {
+            System.IO.File.AppendAllText(FilePath, content + "\n");
+        }
+
+        private List<string> LoadContent()
+        {
+            if (System.IO.File.Exists(FilePath))
+            {
+                return System.IO.File.ReadAllLines(FilePath).ToList();
+            }
+            return new List<string>();
+        }
+    }
+}
---
+++ testCSharp/testCSharp/Models/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace testCSharp.Models
+{
+    public class UserContext
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
